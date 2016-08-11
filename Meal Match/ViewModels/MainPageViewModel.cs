d0f91d9105f9382cab47b844140bad54453aceef
theblueprint415 <justin.chizer@gmail.com>
2016-08-10 18:38:09 -0700

    ViewModel

--- Meal Match/ViewModels/MainPageViewModel.cs
+++ Meal Match/ViewModels/MainPageViewModel.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Template10.Mvvm;
+using System.Collections.ObjectModel;
+using Meal_Match.Models;
+using System.Threading;
+using Template10.Utils;
+using Template10.Common;
+using Windows.UI.Xaml.Navigation;
+using System.Runtime.CompilerServices;
 
 namespace Meal_Match.ViewModels
 {
-    class MainPageViewModel
+    public class MainPageViewModel : ViewModelBase
     {
     }
 }
--- Meal Match/Views/MainPage.xaml.cs
+++ Meal Match/Views/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Meal_Match
+namespace Meal_Match.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,6 +25,7 @@ namespace Meal_Match
         public MainPage()
         {
             this.InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
         }
     }
 }
