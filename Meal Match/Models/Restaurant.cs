d9d3a046dc5824b8430b9fdc4cd95f12324c062f
theblueprint415 <justin.chizer@gmail.com>
2016-08-18 16:57:11 -0700

    Revert "Been awhile"
    This reverts commit 40cf7ba622d537bed6b55cbe3d1e24f40bcc88f8.

--- Meal Match/Models/Restaurant.cs
+++ Meal Match/Models/Restaurant.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,114 +14,10 @@ namespace Meal_Match.Models
             Name = restaurant.name;
             Thumbnail = restaurant.thumb;
             Address = restaurant.location.address;
-            #region Price
-            if(restaurant.price_range == 1)
-            {
-                Price = "$";
-            }
-            else if(restaurant.price_range == 2)
-            {
-                Price = "$$";
-            }
-            else if(restaurant.price_range == 3)
-            {
-                Price = "$$$";
-            }
-            else
-            {
-                Price = "$$$$";
-            }
-            #endregion
-            #region Rating
-
-            // Rating = "\xE1CE \xE1CF \xE7C6"; // Star, Filled Star, Half star
-            if (restaurant.user_rating.aggregate_rating == "0" ||
-                restaurant.user_rating.aggregate_rating == "0.1" ||
-                restaurant.user_rating.aggregate_rating == "0.2" ||
-                restaurant.user_rating.aggregate_rating == "0.3" ||
-                restaurant.user_rating.aggregate_rating == "0.4" )
-            {
-                Rating = "\xE1CE\xE1CE\xE1CE\xE1CE\xE1CE"; //0
-            }
-            else if (restaurant.user_rating.aggregate_rating == "0.5" ||
-                     restaurant.user_rating.aggregate_rating == "0.6" ||
-                     restaurant.user_rating.aggregate_rating == "0.7" ||
-                     restaurant.user_rating.aggregate_rating == "0.8" ||
-                     restaurant.user_rating.aggregate_rating == "0.9" )
-            {
-                Rating = "\xE7C6\xE1CE\xE1CE\xE1CE\xE1CE"; //0.5
-            }
-            else if (restaurant.user_rating.aggregate_rating == "1.0" ||
-                     restaurant.user_rating.aggregate_rating == "1.1" ||
-                     restaurant.user_rating.aggregate_rating == "1.2" ||
-                     restaurant.user_rating.aggregate_rating == "1.3" ||
-                     restaurant.user_rating.aggregate_rating == "1.4" )
-            {
-                Rating = "\xE1CF\xE1CE\xE1CE\xE1CE\xE1CE"; //1
-            }
-            else if (restaurant.user_rating.aggregate_rating == "1.5" ||
-                     restaurant.user_rating.aggregate_rating == "1.6" ||
-                     restaurant.user_rating.aggregate_rating == "1.7" ||
-                     restaurant.user_rating.aggregate_rating == "1.8" ||
-                     restaurant.user_rating.aggregate_rating == "1.9")
-            {
-                Rating = "\xE1CF\xE7C6\xE1CE\xE1CE\xE1CE"; //1.5
-            }
-            else if (restaurant.user_rating.aggregate_rating == "2.0" ||
-                     restaurant.user_rating.aggregate_rating == "2.1" ||
-                     restaurant.user_rating.aggregate_rating == "2.2" ||
-                     restaurant.user_rating.aggregate_rating == "2.3" ||
-                     restaurant.user_rating.aggregate_rating == "2.4")
-            {
-                Rating = "\xE1CF\xE1CF\xE1CE\xE1CE\xE1CE"; //2
-            }
-            else if (restaurant.user_rating.aggregate_rating == "2.5" ||
-                     restaurant.user_rating.aggregate_rating == "2.6" ||
-                     restaurant.user_rating.aggregate_rating == "2.7" ||
-                     restaurant.user_rating.aggregate_rating == "2.8" ||
-                     restaurant.user_rating.aggregate_rating == "2.9")
-            {
-                Rating = "\xE1CF\xE1CF\xE7C6\xE1CE\xE1CE"; //2.5
-            }
-            else if (restaurant.user_rating.aggregate_rating == "3.0" ||
-                     restaurant.user_rating.aggregate_rating == "3.1" ||
-                     restaurant.user_rating.aggregate_rating == "3.2" ||
-                     restaurant.user_rating.aggregate_rating == "3.3" ||
-                     restaurant.user_rating.aggregate_rating == "3.4")
-            {
-                Rating = "\xE1CF\xE1CF\xE1CF\xE1CE\xE1CE"; //3
-            }
-            else if (restaurant.user_rating.aggregate_rating == "3.5" ||
-                     restaurant.user_rating.aggregate_rating == "3.6" ||
-                     restaurant.user_rating.aggregate_rating == "3.7" ||
-                     restaurant.user_rating.aggregate_rating == "3.8" ||
-                     restaurant.user_rating.aggregate_rating == "3.9")
-            {
-                Rating = "\xE1CF\xE1CF\xE1CF\xE7C6\xE1CE"; //3.5
-            }
-            else if (restaurant.user_rating.aggregate_rating == "4.0" ||
-                restaurant.user_rating.aggregate_rating == "4.1" ||
-                restaurant.user_rating.aggregate_rating == "4.2" || 
-                restaurant.user_rating.aggregate_rating == "4.3" )
-            {
-                Rating = "\xE1CF\xE1CF\xE1CF\xE1CF\xE1CE"; //4
-            }
-            else if (restaurant.user_rating.aggregate_rating == "4.4" ||
-                     restaurant.user_rating.aggregate_rating == "4.5" ||
-                     restaurant.user_rating.aggregate_rating == "4.6" ||
-                     restaurant.user_rating.aggregate_rating == "4.7" ||
-                     restaurant.user_rating.aggregate_rating == "4.8" ||
-                     restaurant.user_rating.aggregate_rating == "4.9" )
-            {
-                Rating = "\xE1CF\xE1CF\xE1CF\xE1CF\xE7C6"; //4.5
-            }
-            else
-            {
-                Rating = "\xE1CF\xE1CF\xE1CF\xE1CF\xE1CF"; //5
-            }
-            #endregion
-
+            Price = restaurant.price_range.ToString();
         }
+
+
 
         public Restaurant2 restaurant { get; set; }
 
@@ -130,7 +25,6 @@ namespace Meal_Match.Models
         public string Thumbnail { get; set; }
         public string Address { get; set; }
         public string Price { get; set; }
-        public string Rating { get; set; }
 
     }
 }
--- Meal Match/ViewModels/MainPageViewModel.cs
+++ Meal Match/ViewModels/MainPageViewModel.cs
@@ -25,11 +25,11 @@ namespace Meal_Match.ViewModels
         public MainPageViewModel()
         {
             _dataService = new Services.DataService();
-            //PropertyChanged += async (s,e) =>
-            //{
+            PropertyChanged += async (s,e) =>
+            {
                 _cancellationTokens.ForEach(x => x.Cancel());
-                DisplayResults();
-            //};
+                await DisplayResults();
+            };
 
         }
 
@@ -43,10 +43,10 @@ namespace Meal_Match.ViewModels
             {
                 DispatcherWrapper.Current().Dispatch(() => 
                     {
-                    Restaurants.Clear();
+                    restaurants.Clear();
                     foreach (var restaurant in restaurants)
                     {
-                        Restaurants.Add(restaurant);
+                        restaurants.Add(restaurant);
                     }
                 });
                 }
@@ -56,6 +56,126 @@ namespace Meal_Match.ViewModels
         public ObservableCollection<Restaurant> Restaurants { get; } = new ObservableCollection<Restaurant>();
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public async Task SlotMachine()
+        //{
+        //    int tempTime1 = randomNumber1();
+        //    await DelaySlot1(tempTime1);
+
+        //    int tempTime2 = randomNumber2();
+        //    await DelaySlot2(tempTime2);
+
+        //    int tempTime3 = randomNumber3();
+        //    await DelaySlot3(tempTime3);
+        //}
+
+        //public int randomNumber1()
+        //{
+        //    Random randomNum1 = new Random();
+        //    int randomTime1 = randomNum1.Next(0, 100);
+
+
+        //    //testnum.Text = randomTime1.ToString();
+        //    return randomTime1;
+
+        //}
+
+        //public int randomNumber2()
+        //{
+        //    Random randomNum2 = new Random();
+        //    int randomTime2 = randomNum2.Next(0, 100);
+
+
+        //    //testnum.Text = randomTime2.ToString();
+        //    return randomTime2;
+
+        //}
+
+        //public int randomNumber3()
+        //{
+        //    Random randomNum3 = new Random();
+        //    int randomTime3 = randomNum3.Next(0, 100);
+
+
+        //    //testnum.Text = randomTime3.ToString();
+        //    return randomTime3;
+
+        //}
+
+
+        //public async Task DelaySlot1(int randomTime1)
+        //{
+        //    await Task.Delay(TimeSpan.FromSeconds(3));
+        //    if (randomTime1 % 2 == 0)
+        //    {
+        //        imageslot1.Source = new BitmapImage(new Uri("ms-appx:///Assets/Matrix0.PNG"));
+        //    }
+        //    else if (randomTime1 % 2 == 1)
+        //    {
+        //        imageslot1.Source = new BitmapImage(new Uri("ms-appx:///Assets/Matrix1.PNG"));
+        //    }
+        //    else
+        //    {
+        //        imageslot1.Source = new BitmapImage(new Uri("ms-appx:///Assets/MatrixX.PNG"));
+        //    }
+
+        //    await Task.CompletedTask;
+        //}
+
+
+        //public async Task DelaySlot2(int randomTime2)
+        //{
+        //    await Task.Delay(TimeSpan.FromSeconds(1));
+
+        //    if (randomTime2 % 2 == 0)
+        //    {
+        //        imageslot2.Source = new BitmapImage(new Uri("ms-appx:///Assets/Matrix0.PNG"));
+        //    }
+        //    else if (randomTime2 % 2 == 1)
+        //    {
+        //        imageslot2.Source = new BitmapImage(new Uri("ms-appx:///Assets/Matrix1.PNG"));
+        //    }
+        //    else
+        //    {
+        //        imageslot2.Source = new BitmapImage(new Uri("ms-appx:///Assets/MatrixX.PNG"));
+        //    }
+
+        //    await Task.CompletedTask;
+
+        //}
+
+        //public async Task DelaySlot3(int randomTime3)
+        //{
+        //    await Task.Delay(TimeSpan.FromSeconds(1));
+
+        //    if (randomTime3 % 2 == 0)
+        //    {
+        //        imageslot3.Source = new BitmapImage(new Uri("ms-appx:///Assets/Matrix0.PNG"));
+        //    }
+        //    else if (randomTime3 % 2 == 1)
+        //    {
+        //        imageslot3.Source = new BitmapImage(new Uri("ms-appx:///Assets/Matrix1.PNG"));
+        //    }
+        //    else
+        //    {
+        //        imageslot3.Source = new BitmapImage(new Uri("ms-appx:///Assets/MatrixX.PNG"));
+        //    }
+
+        //    await Task.CompletedTask;
+
+        //}
 
 
     }
